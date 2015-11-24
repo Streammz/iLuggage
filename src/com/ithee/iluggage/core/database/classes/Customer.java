8c27ed8563f5a04fa76bc1a9a710dbf0dac7a2fa
rowanvi <rowan.visser@hotmail.com>
2015-11-24 12:40:24 +0100

    LostLuggage is af, alleen luggage word niet ingevoerd door datetime

---
+++ src/com/ithee/iluggage/core/database/classes/Customer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ithee.iluggage.core.database.classes;
+
+/**
+ *
+ * @author Rowan
+ */
+public class Customer {
+    public int id;
+    public String name;
+    public String email;
+    public String phone;
+    
+}
